f2fd027e9e847858ef135026de7c08f814bcb740
ntwari-egide <ntwariegide2@gmail.com>
2021-12-16 15:50:12 +0200

    ft(city service): implement all required tests

--- src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
+++ src/test/java/rw/ac/rca/termOneExam/service/CityServiceTest.java
@@ -1,5 +1,92 @@
 package rw.ac.rca.termOneExam.service;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import rw.ac.rca.termOneExam.domain.City;
+import rw.ac.rca.termOneExam.dto.CreateCityDTO;
+import rw.ac.rca.termOneExam.repository.ICityRepository;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CityServiceTest {
+
+    @Mock
+    private ICityRepository cityRepositoryMock;
+
+    @InjectMocks
+    private CityService cityService;
+
+    @Test
+    public void getAll_Success() {
+
+        when(cityRepositoryMock.findAll()).thenReturn(Arrays.asList(new City("Kigali",10),
+                new City("Nyabihu",10)));
+
+        assertEquals("Kigali",cityService.getAll().get(0).getName());
+        assertEquals("Nyabihu",cityService.getAll().get(1).getName());
+        assertEquals(50,cityService.getAll().get(1).getFahrenheit());
+    }
+
+    @Test
+    public void getById_Success() {
+
+        when(cityRepositoryMock.findById(100L)).thenReturn(Optional.of(new City("Kigali",10)));
+
+        assertEquals("Kigali",cityService.getById(100).get().getName());
+        assertEquals(10,cityService.getById(100).get().getWeather());
+    }
+
+//    @Test
+//    public void getById_404() {
+//
+//        when(cityRepositoryMock.findById(1000L)).thenReturn(Optional.ofNullable(null));
+//
+//        System.out.println(cityService.getById(1000L));
+//        assertEquals(false,cityService.getById(1000).isPresent());
+//    }
+
+
+    @Test
+    public void saveCity_Success () {
+        CreateCityDTO city = new CreateCityDTO();
+        city.setName("Kayonza");
+        city.setWeather(20);
+
+        City savedCity = new City();
+        savedCity.setName("Kayonza");
+        savedCity.setWeather(20);
+        savedCity.setFahrenheit(68);
+
+        when(cityRepositoryMock.save(any())).thenReturn(savedCity);
+
+
+        City saved = cityService.save(city);
+
+        assertEquals("Kayonza", savedCity.getName());
+        assertEquals(68, savedCity.getFahrenheit());
+
+    }
+
+    @Test
+    public void existsByName_Success () {
+        CreateCityDTO city = new CreateCityDTO();
+        city.setName("Kigali");
+        city.setWeather(20);
+
+        when(cityRepositoryMock.existsByName("Kigali")).thenReturn(true);
+
+        assertEquals(true, cityService.existsByName("Kigali"));
+
+    }
+
 
 }
